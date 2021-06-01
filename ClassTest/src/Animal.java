236da9704f9f4c4b01bc56b57ee38d64d79303e1
EllenJiaping <2500840435@qq.com>
2021-06-01 12:32:24 +0800

    change field and method

--- ClassTest/src/Animal.java
+++ ClassTest/src/Animal.java
@@ -1,4 +1,11 @@
 public class Animal {
+    String name;
+    int age;
+    int sex;
+
+    void sleep() {
+        System.out.println("sleep");
+    }
 
     public static void main(String[] args) {
 
